9f885d3bce718121b3b2f25b0b88580045b4e236
Victor Nascimento Bakke <gipphe@gmail.com>
2024-09-27 08:30:37 +0200

    chore: make lint command that takes into account language extensions

--- flake.nix
+++ flake.nix
@@ -10,22 +10,49 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
+        lint = pkgs.writeShellApplication {
+          name = "lint";
+          runtimeInputs = with pkgs; [
+            haskellPackages.hlint
+            yq
+          ];
+          text = ''
+            exts=$(yq -r '.["default-extensions"] | .[]' package.yaml | xargs)
+            prunedExts=('DataKinds')
+            filteredExts=()
+            for ext in $exts; do
+              for pruneExt in "''${prunedExts[@]}"; do
+                if test "$ext" = "$pruneExt"; then
+                  continue
+                fi
+              done
+            done
+            extparam=()
+            for ext in "''${filteredExts[@]}"; do
+              extparam+=("-X $ext")
+            done
+            IFS=''' hlint "''${extparam[@]}" "$@"
+          '';
+        };
         package = {
           name = "production-calc";
           root = ./.;
           modifier =
             drv:
             pkgs.haskell.lib.addBuildTools drv (
-              with pkgs;
-              (
-                [ nixfmt-rfc-style ]
-                ++ (with haskellPackages; [
-                  hlint
-                  fourmolu
-                  cabal-install
-                  cabal-fmt
-                  hpack
-                ])
+              [ lint ]
+              ++ (
+                with pkgs;
+                (
+                  [ nixfmt-rfc-style ]
+                  ++ (with haskellPackages; [
+                    hlint
+                    fourmolu
+                    cabal-install
+                    cabal-fmt
+                    hpack
+                  ])
+                )
               )
             );
         };
